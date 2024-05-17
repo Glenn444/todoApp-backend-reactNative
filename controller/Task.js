ba2a80a0dd856ec34946ff682307ceb1fb06be4f
Glenn444 <gmark1586@gmail.com>
2024-05-17 13:12:13 +0300

    Added CRUD operations

--- controller/Task.js
+++ controller/Task.js
@@ -10,9 +10,29 @@ const findAllTasks = async(req, res)=>{
     const alltasks = await Task.find({});
     res.status(200).json(alltasks);
 }
-
-
+const getTask = async(req, res)=>{
+    const {id:taskId} = req.params;
+    const task = await Task.findOne({_id:taskId});
+    res.status(200).json(task);
+}
+const findAndUpdate = async (req, res)=>{
+    const {id:taskId} = req.params;
+    const task = await Task.findOneAndUpdate({_id:taskId},req.body,{
+        new:true,
+        runValidators:true,
+    });
+    res.status(200).json(task)
+}
+const findAndDelete = async (req, res)=>{
+    const {id:taskId} = req.params;
+    console.log(taskId);
+    await Task.findOneAndDelete({_id:taskId});
+    res.status(200).json({message:"Task deleted successfully"});
+}
 module.exports = {
     createTask,
-    findAllTasks
+    findAllTasks,
+    findAndUpdate,
+    findAndDelete,
+    getTask
 }
